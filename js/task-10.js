ff345d26d7ea9fc80e40cdf1c923b5e06c7e2e63
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-04 18:18:28 +0200

    виконано 10 завдання

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,31 @@
+const controls = document.querySelector('#controls');
+const input = controls.firstElementChild;
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
 
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
+function createBoxes(amount) {
+  amount = input.value;
+  const arrDivs = [];
+  for (let i = 0; i <= amount - 1; i++) {
+   const boxSizeValue = (i * 10 + 30);
+    const box = document.createElement('div');
+    box.style.width = `${boxSizeValue}px`;
+    box.style.height = `${boxSizeValue}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    arrDivs.push(box);
+  };
+  const divsCollection = arrDivs.map(div => div.outerHTML).join('');
+  boxes.insertAdjacentHTML('afterBegin', divsCollection);
+};
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+};
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}ж
